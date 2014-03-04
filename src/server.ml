c80a178f6f288b797a34118b040e480dd53ea7e9
Dominic Price <dominic.price@nottingham.ac.uk>
2014-03-04 18:09:34 +0000

    Made multi-threaded with Lwt threads

--- src/server.ml
+++ src/server.ml
@@ -1,6 +1,10 @@
-module Conversation : sig
-  val start : Unix.file_descr -> string -> (string -> unit) -> unit
-end = struct
+module Lio = Lwt_io
+module Lu = Lwt_unix
+
+let (>>=) = Lwt.(>>=)
+let (>>) a b = a >>= fun () -> b 
+
+module Conversation = struct
   
   let response_220 = "220 receive-mail SMTP Server"
   let response_250 = "250 OK"
@@ -17,23 +21,20 @@ end = struct
  
   let write_response out_c id response = 
     LOG "[%s] Sent: %s" id response LEVEL DEBUG;
-    output_string out_c response;
-    output_string out_c "\r\n";
-    flush out_c
+    Lio.write out_c response >>
+    Lio.write out_c "\r\n" >>
+    Lio.flush out_c
     
   let read_data id in_c out_c =
-    write_response out_c id response_354;
+    write_response out_c id response_354 >>
     let module S = Core.Core_string in
     let module B = Buffer in
-    let to_read = 3 in
-    let ibuf = S.make to_read (char_of_int 0) in
-    let buf = B.create to_read in
+    let buf = B.create 0 in
     let rec read () = 
-      let data = input in_c ibuf 0 to_read |>
-        String.sub ibuf 0 in
+      Lio.read ~count:80 in_c >>= fun data ->
       B.add_string buf data;
       match S.is_suffix (B.contents buf) "\r\n.\r\n" with
-      | true -> B.sub buf 0 (B.length buf - 5)
+      | true -> Lwt.return (B.sub buf 0 (B.length buf - 5))
       | false -> read () in
     read ()
 
@@ -44,18 +45,18 @@ end = struct
     |> fun subs -> Re.get subs 1
     |> String.lowercase
     |> function
-      | "helo" | "ehlo" -> Helo
-      | "mail" -> Mail (Re.get subs 2)
-      | "rcpt" -> Rcpt (Re.get subs 2)
-      | "data" -> Data (data_reader ())
-      | "quit" -> Quit
-      | _ -> Unknown
+      | "helo" | "ehlo" -> Lwt.return Helo
+      | "mail" -> Lwt.return (Mail (Re.get subs 2))
+      | "rcpt" -> Lwt.return (Rcpt (Re.get subs 2))
+      | "data" -> data_reader () >>= fun data -> Lwt.return (Data (data))
+      | "quit" -> Lwt.return Quit
+      | _ -> Lwt.return Unknown
 
   let rec process_request ?previous:(previous) callback buf id in_c out_c =
     let wr = write_response out_c id in 
-    let line = input_line in_c in
+    Lio.read_line in_c >>= fun line ->
     LOG "[%s] Received: %s" id line LEVEL DEBUG;
-    let req = parse_request (fun () -> read_data id in_c out_c) line in 
+    parse_request (fun () -> read_data id in_c out_c) line >>= fun req -> 
     (match req with
     | Helo -> wr response_250
     | Mail from ->
@@ -74,43 +75,46 @@ end = struct
       Buffer.clear buf;
       wr response_250
     | Quit -> wr response_221
-    | Unknown -> ());
+    | Unknown -> Lwt.return ()) >>
     match req with
-    | Quit -> ()
+    | Quit -> Lwt.return ()
     | r -> process_request ~previous:r callback buf id in_c out_c
   
   let start fd id callback = 
     LOG "[%s] Accepted a connection" id LEVEL DEBUG;
-    let in_c = Unix.in_channel_of_descr fd in
-    let out_c = Unix.out_channel_of_descr fd in
+    let in_c = Lio.of_fd Lio.Input fd in
+    let out_c = Lio.of_fd Lio.Output fd in
     let buf = Buffer.create 80 in
-    write_response out_c id response_220;
-    process_request callback buf id in_c out_c;
-    close_in in_c;
-    try close_out out_c with _ -> ()
+    write_response out_c id response_220 >>
+    process_request callback buf id in_c out_c >>
+    try_lwt Lio.close in_c with _ -> Lwt.return () >>
+    try_lwt Lio.close out_c with _ -> Lwt.return () 
   
 end
 
 let sockaddr_to_string = function
-  | Unix.ADDR_UNIX s -> s
-  | Unix.ADDR_INET (a, p) ->
+  | Lu.ADDR_UNIX s -> s
+  | Lu.ADDR_INET (a, p) ->
     Printf.sprintf "%s:%i" (Unix.string_of_inet_addr a) p 
     
-let start addr port callback =
-  let a = Unix.ADDR_INET (Unix.inet_addr_of_string addr, port) in
-  let sock = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-  Unix.setsockopt sock Unix.SO_REUSEADDR true;
-  try
-    Unix.bind sock a ;
-    Unix.listen sock 3;
-    while true do
-      LOG "Waiting for a connection" LEVEL DEBUG;
-      let fd, a = Unix.accept sock in
+let start ?max_pending:(max_pending=3) inet_addr port callback =
+  let address = Unix.ADDR_INET (inet_addr, port) in
+  let socket = Lu.socket Lu.PF_INET Lu.SOCK_STREAM 0 in
+  Lu.setsockopt socket Lu.SO_REUSEADDR true;
+  Lu.bind socket address;
+  Lu.listen socket max_pending;
+  let rec listen () =
+    LOG "Waiting for a connection" LEVEL DEBUG;
+    Lu.accept socket >>= fun (fd, a) -> 
+    Lwt.join [
+      listen ();
+      (Lu.yield () >>
       let id = sockaddr_to_string a in
-      Conversation.start fd id callback;
-      LOG "[%s] Connection closed" (sockaddr_to_string a) LEVEL DEBUG;
-    done
-  with e -> 
-    Unix.close sock;
-    raise e
+      Conversation.start fd id callback >>
+      (LOG "[%s] Connection closed" id LEVEL DEBUG;
+      Lwt.return ()));
+    ] in
+  listen () 
+  |> (fun t -> Lwt.on_cancel t (fun () -> Lu.close socket |> ignore); t)
+    
   
--- src/server.mli
+++ src/server.mli
@@ -1,1 +1,2 @@
-val start : string -> int -> (string -> unit) -> unit
+val start : 
+  ?max_pending:int -> Unix.inet_addr -> int -> (string -> unit) -> unit Lwt.t
